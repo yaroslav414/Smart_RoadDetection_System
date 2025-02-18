22220987a09b6cbfb3dbd39184bb215bc3650292
yaroslav414 <smartec414+2@gmail.com>
2025-02-18 23:14:50 +0300

    Add files via upload

---
+++ src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -0,0 +1,12 @@
+package com.surendramaran.yolov8tflite
+
+object Constants {
+    const val MODEL_PATH = "Base_Potholes_2-4_float32.tflite"
+    const val LABELS_PATH = "labels.txt"
+    const val IP_ADDRESS = "http://192.168.1.60:8000"
+    const val WEBSITE_ADDRESS = "http://192.168.1.60:5000"
+}//REMEMBER TO CHANGE NETWORK_SECURITY_CONFIG
+// my phone internet ip 192.168.147.87
+//my home ip 192.168.1.35
+//elayan phone IP 192.168.92.87
+//NETSECCONFIG
